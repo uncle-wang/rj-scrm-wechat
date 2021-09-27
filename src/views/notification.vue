bbda0928825648ae2a39d44575cc4fbf0c502154
wangjin <wangjin.js@gmail.com>
2021-09-28 01:06:30 +0800

    数据格式化

--- src/views/notification.vue
+++ src/views/notification.vue
@@ -58,16 +58,36 @@ export default {
       const taskInfo = this.tasks[0];
       const contents = [];
       // 文本
-      if (taskInfo.pushTextInfo) {
-        contents.push({ extType: MSGTYPE.TEXT, textContent: taskInfo.pushTextInfo });
+      if (taskInfo.textContent) {
+        contents.push({ extType: MSGTYPE.TEXT, textContent: taskInfo.textContent });
       }
       // 图片
       if (taskInfo.imageMediaId) {
         contents.push({ extType: MSGTYPE.IMAGE, imageMediaId: taskInfo.imageMediaId });
       }
+      // 链接
+      if (taskInfo.linkUrl && taskInfo.linkTitle) {
+        contents.push({
+          extType: MSGTYPE.LINK,
+          linkUrl: taskInfo.linkUrl,
+          linkTitle: taskInfo.linkTitle,
+          linkDesc: taskInfo.linkDesc,
+          linkPicurl: taskInfo.linkPicurl,
+        });
+      }
       // 文件
       if (taskInfo.fileMediaId) {
         contents.push({ extType: MSGTYPE.FILE, fileMediaId: taskInfo.fileMediaId });
+      }
+      // 小程序
+      if (taskInfo.miniAppid && taskInfo.miniTitle && taskInfo.miniPage && taskInfo.miniPicMediaId) {
+        contents.push({
+          extType: MSGTYPE.MINIPROGRAM,
+          miniAppid: taskInfo.miniAppid,
+          miniTitle: taskInfo.miniTitle,
+          miniPage: taskInfo.miniPage,
+          miniPicMediaId: taskInfo.miniPicMediaId,
+        });
       }
       return contents;
     },
